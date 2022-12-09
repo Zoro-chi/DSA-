3bc45e8f28bf37de3500fd4aa9f64cb92e4e9b8c
Zoro-chi <jagbetuyi001@gmail.com>
2022-12-09 19:18:08 +0100

    count Chars

---
+++ 100Devs-Standup/count_Chars_String/countChars.ts
@@ -0,0 +1,26 @@
+// Count all the occurring characters in a string. If you have a string like aba, then the result should be {'a': 2, 'b': 1}.
+// What if the string is empty? Then the result should be empty object literal, {}.
+
+// *SOLUTION
+
+const countChars = (str: string) => {
+	interface answerType {
+		[key: string]: any;
+	}
+
+	// let answer: answerType = {} as any as answerType;
+	let answer: answerType = {};
+
+	for (const letter of str) {
+		if (answer[letter]) {
+			answer[letter]++;
+		} else {
+			answer[letter] = 1;
+		}
+	}
+	return answer;
+};
+
+console.log(countChars("aba"), "{'a': 2, 'b': 1}");
+console.log(countChars("dog"), "{'d': 1, 'o': 1, 'g': 1}");
+console.log(countChars("abba"), "{'a': 2, 'b': 2}");
